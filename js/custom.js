8f0ce743d1279bd229c7854c0963d393078a5d56
dinesh <dinesh.velhal@gmail.com>
2019-07-16 09:10:10 +0530

    advanced.html updated

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+var parser = new DOMParser();
+var domString = '<template id="book_template"><style>.book-label{-webkit-box-shadow:0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;box-shadow:0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;border-color:#2780E3 !important;border:1px solid rgba(0,0,0,0.125);background-color:#fff;-webkit-box-orient:vertical;-webkit-box-direction:normal;padding:10px;margin:5px;text-align:center}.rating{color:#3FB618}</style><table><tr><td class="book-label" id="book_name">Sapiens: A Brief History of the Humankind</td><td class="book-label rating" id="star_rating">****</td></tr></table> </template>';
+
+var html = parser.parseFromString(domString, 'text/html');
+
+var template = html.head.firstChild;
+
+var shadow = document.querySelector('#book').attachShadow({
+    mode: 'open'
+});
+var clone = document.importNode(template.content, true);
+shadow.appendChild(clone);
